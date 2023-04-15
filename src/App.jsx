8e8ae8f6466f33ef22eeeb50af32bed738de2b0e
aliseyedi <solafiali657@gmail.com>
2023-04-15 15:36:50 +0330

    chore (tailwindcss) : add prettier plugin tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold  text-red-500">Hello world!</h1>
+      <h1 className="text-3xl font-bold  text-red-500 ">Hello world!</h1>
     </div>
   );
 }
