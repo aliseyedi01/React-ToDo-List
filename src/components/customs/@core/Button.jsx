e9549a6e39992885b12d436673ff64e89797fbf0
aliseyedi <solafiali657@gmail.com>
2023-04-15 20:54:07 +0330

    feat ( button ) : add Button Component

--- src/components/customs/@core/Button.jsx
+++ src/components/customs/@core/Button.jsx
@@ -1,5 +1,5 @@
 import tw from "tailwind-styled-components";
 
-const Button = tw.button`rounded-md bg-red-500 px-6 py-2 font-mono `;
+const Button = tw.button`rounded-md font-mono py-2 bg-red-500 px-6   `;
 
 export default Button;
