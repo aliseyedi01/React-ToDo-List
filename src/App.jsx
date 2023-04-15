b500a34ad6ca7afdfff6548ade0f2642c9f03368
aliseyedi <solafiali657@gmail.com>
2023-04-15 20:21:05 +0330

    feat ( styled ) : add tailwind styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { FiSun, FiMoon } from "react-icons/fi";
+import { Button } from "./components/customs/@core";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold  text-red-500 ">
         <FiSun />
         ToDo List App
       </h1>
+      <Button>Ali</Button>
     </div>
   );
 }
---
+++ src/components/customs/@core/Button.jsx
@@ -0,0 +1,5 @@
+import tw from "tailwind-styled-components";
+
+const Button = tw.button`rounded-md bg-red-500 px-6 py-2 font-mono `;
+
+export default Button;
