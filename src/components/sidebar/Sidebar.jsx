b3619b7c0f0e0b225f9fc0be564d60e8bd7c6836
aliseyedi <solafiali657@gmail.com>
2023-04-16 10:33:10 +0330

    feat ( TaskItem ) : add TaskItem Component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Menu } from "../customs/@core";
 import DarkMode from "./DarkMode";
+import TaskItem from "./TaskItem";
 
 export default function Sidebar() {
   return (
     <Menu>
-      <p className=" dark:bg-dark_bg_Second   mt-2 w-full   text-center  font-Rubik text-3xl font-medium  text-red-500  dark:text-gray-400">
+      <p className=" dark:bg-dark_bg_Second   mt-2 w-full text-center  font-Rubik text-3xl font-medium  text-red-500  dark:text-gray-400">
         TODO APP
       </p>
-
+      <TaskItem />
       <DarkMode />
     </Menu>
   );
---
+++ src/components/sidebar/TaskItem.jsx
@@ -0,0 +1,50 @@
+import { IoIosToday } from "react-icons/io";
+import { BsListTask } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
+import { MdRemoveDone, MdDoneAll } from "react-icons/md";
+
+const Items = [
+  {
+    name: "Today's tasks",
+    path: "/today",
+    icon: <IoIosToday />,
+  },
+  {
+    name: "All Tasks",
+    path: "/",
+    icon: <BsListTask />,
+  },
+  {
+    name: "Important",
+    path: "/important",
+    icon: <AiFillStar />,
+  },
+  {
+    name: "Done",
+    path: "/Done",
+    icon: <MdDoneAll />,
+  },
+  {
+    name: "ToDo",
+    path: "/ToDo",
+    icon: <MdRemoveDone />,
+  },
+];
+
+export default function TaskItem() {
+  return (
+    <div className="mt-8 h-full items-start self-start">
+      <ul>
+        {Items.map((item, i) => (
+          <li
+            key={i}
+            className=" flex w-full cursor-pointer items-center gap-2 px-4 py-2 text-rose-600 transition hover:text-rose-800 dark:text-slate-300 dark:hover:text-slate-100"
+          >
+            {item.icon}
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
