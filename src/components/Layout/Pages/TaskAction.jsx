e5b65a51231644b73015264bf2a4f4ab91f20153
aliseyedi <solafiali657@gmail.com>
2023-04-18 00:10:35 +0330

    style ( TaskAction ) : Change Margin & add Title to Button

--- src/components/Layout/Pages/TaskAction.jsx
+++ src/components/Layout/Pages/TaskAction.jsx
@@ -6,25 +6,25 @@ import { FiEdit } from "react-icons/fi";
 
 export default function TaskAction() {
   return (
-    <div className="flex items-center justify-between py-2">
+    <div className="flex items-center justify-between pt-1">
       {/* category */}
-      <button className="rounded-md bg-Light_Secondary px-3 py-1 dark:bg-Dark_Secondary">
+      <button className="rounded-md bg-Light_Secondary px-3 py-1  dark:bg-Dark_Secondary">
         <span className="text-Light_OnSecondary dark:text-Light_OnSecondary ">Home</span>
       </button>
       {/* button action */}
       <div className="flex items-center gap-1 text-2xl text-Light_OnSurface dark:text-Dark_OnSurface ">
-        <button className="flex gap-2">
+        <button className="flex gap-2" title="Done">
           <BsCheckSquareFill />
           {/* <ImCheckboxUnchecked /> */}
         </button>
-        <button className="flex gap-2">
+        <button className="flex gap-2" title="Important">
           <AiFillStar />
           {/* <AiOutlineStar /> */}
         </button>
-        <button>
+        <button title="Delete">
           <RiDeleteBin5Fill />
         </button>
-        <button>
+        <button title="Edit">
           <FiEdit />
         </button>
       </div>
