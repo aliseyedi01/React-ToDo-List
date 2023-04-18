f482759ca4ffa50cdf6b267571b90f13488d2ece
aliseyedi <solafiali657@gmail.com>
2023-04-18 11:31:12 +0330

    feat ( Pages ) : add category list to Pages

--- src/components/sidebar/category/ListCategories.jsx
+++ src/components/sidebar/category/ListCategories.jsx
@@ -3,33 +3,44 @@ import { AiFillHome } from "react-icons/ai";
 import { BsPersonWorkspace } from "react-icons/bs";
 import { MdSchool } from "react-icons/md";
 import AddCategory from "./NewCategory";
+import { NavLink, useLocation } from "react-router-dom";
 
 const categories = [
   {
-    name: "Home",
+    name: "House",
+    path: "/house",
     icon: <AiFillHome />,
   },
   {
     name: "Work",
+    path: "/work",
     icon: <BsPersonWorkspace />,
   },
   {
     name: "School",
+    path: "/school",
     icon: <MdSchool />,
   },
 ];
 
 export default function ListCategories({ isCategoryOpen }) {
+  const route = useLocation();
+  const currentPath = route.pathname;
+
   return (
     <div className={!isCategoryOpen ? "visible" : "hidden"}>
-      <ul className="transition-none duration-0">
+      <ul className="pt-1 transition-none duration-0">
         {categories.map((category, i) => (
-          <li
-            key={i}
-            className="flex cursor-pointer items-center gap-2 pl-4 pt-3 transition-none duration-0 hover:text-rose-600 dark:hover:text-slate-100  "
-          >
-            <div className="transition-none duration-0">{category.icon}</div>
-            {category.name}
+          <li key={i}>
+            <NavLink
+              to={category.path}
+              className={`flex cursor-pointer items-center gap-2 py-2 pl-4 transition-none duration-0 hover:text-rose-600 dark:hover:text-slate-100 ${
+                currentPath === category.path ? "rounded-lg bg-gray-300 dark:bg-indigo-800" : ""
+              } `}
+            >
+              <div className="transition-none duration-0">{category.icon}</div>
+              {category.name}
+            </NavLink>
           </li>
         ))}
       </ul>
