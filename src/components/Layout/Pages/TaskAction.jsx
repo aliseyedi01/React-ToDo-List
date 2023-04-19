4930b6141351491e2c8909b068ef4a299926f647
aliseyedi <solafiali657@gmail.com>
2023-04-20 01:12:29 +0330

    feat ( TaskAction ) : Dynamic name  category Button

--- src/components/Layout/Pages/TaskAction.jsx
+++ src/components/Layout/Pages/TaskAction.jsx
@@ -5,18 +5,17 @@ export default function TaskAction({ task }) {
   return (
     <div className="flex items-center justify-between pt-1">
       {/* category */}
-      <button className="rounded-md bg-Light_Secondary px-3 py-1  dark:bg-Dark_Secondary">
-        <span className="text-Light_OnSecondary dark:text-Light_OnSecondary ">Home</span>
+      <button className="rounded-md bg-Light_Secondary/[0.85] px-3 py-1  dark:bg-Dark_Secondary">
+        <span className="text- text-sm text-Light_OnSecondary dark:text-Light_OnSecondary md:text-base ">
+          {task.category}
+        </span>
       </button>
 
       {/* button action */}
       <div className="flex items-center gap-1 text-2xl text-Light_OnSurface dark:text-Dark_OnSurface ">
         <DoneButton task={task} />
-
         <ImportButton task={task} />
-
         <DeleteButton task={task} />
-
         <EditButton task={task} />
       </div>
     </div>
