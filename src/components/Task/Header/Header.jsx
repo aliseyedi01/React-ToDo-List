8cb9a7369e7d78ecdba462647b98542145ec7fed
aliseyedi <solafiali657@gmail.com>
2023-04-19 15:26:18 +0330

    style (sidebar) : Add a glass background to sidebar

--- src/components/Task/Header/Header.jsx
+++ src/components/Task/Header/Header.jsx
@@ -10,7 +10,7 @@ import { MenuContext } from "../../../State/menuReducer";
 export default function Header() {
   const { state, dispatch } = useContext(MenuContext);
 
-  console.log("state header ", state);
+  // console.log("state header ", state.isOpenMenu);
 
   return (
     <header className="flex h-16 w-full items-center justify-between gap-2 bg-Light_Surface/50  px-3  dark:bg-Dark_Surface/95">
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,24 +6,29 @@ import { MenuContext } from "../../State/menuReducer";
 export default function Sidebar() {
   const { state, dispatch } = useContext(MenuContext);
 
-  console.log("state sidebar ", state);
+  // console.log("state sidebar ", state.isOpenMenu);
 
   return (
-    <div
-      className={`z-20  h-screen w-60 items-center justify-start  bg-Light_Surface  py-4 text-Light_OnSurface dark:bg-Dark_Surface dark:text-Dark_OnSurface max-md:fixed  md:block  md:w-[20%] ${
-        state.isOpenMenu ? "" : "hidden"
-      }`}
-    >
-      <div className="flex h-full  flex-col">
-        <p
-          className=" dark:bg-dark_bg_Second   mt-2 w-full text-center  font-Rubik text-3xl font-medium  text-Light_OnSurface  dark:text-Dark_OnSurface"
-          onClick={() => dispatch({ type: "CLOSE_MENU" })}
-        >
-          TODO APP
-        </p>
-        <TaskItem />
-        <Categories />
+    <>
+      <div
+        className={`fixed left-0 top-0 z-10  h-full w-full bg-slate-600/[.2] backdrop-blur-sm ${
+          state.isOpenMenu ? "" : "hidden"
+        }`}
+        onClick={() => dispatch({ type: "CLOSE_MENU" })}
+      ></div>
+      <div
+        className={`z-40  h-screen w-60 items-center justify-start bg-Light_Surface py-4  text-Light_OnSurface  shadow-md shadow-gray-400 dark:bg-Dark_Surface dark:text-Dark_OnSurface max-md:fixed  md:block  md:w-[20%] ${
+          state.isOpenMenu ? "" : "hidden"
+        }`}
+      >
+        <div className="flex h-full  flex-col">
+          <p className=" dark:bg-dark_bg_Second   mt-2 w-full text-center  font-Rubik text-3xl font-medium  text-Light_OnSurface  dark:text-Dark_OnSurface">
+            TODO APP
+          </p>
+          <TaskItem />
+          <Categories />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
