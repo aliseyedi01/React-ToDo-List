084b5c991cd68acd0ee7411de77d428058132491
aliseyedi <solafiali657@gmail.com>
2023-04-19 21:56:04 +0330

    feat ( taskToDo ) : add task uncomplete filtering to component

--- src/components/Pages/TaskToDo.jsx
+++ src/components/Pages/TaskToDo.jsx
@@ -1,5 +1,10 @@
+import { useContext } from "react";
 import LayoutPages from "../Layout/Pages/LayoutPages";
+import { TaskContext } from "../../State/taskReducer";
 
 export default function TaskDone() {
-  return <LayoutPages title="ToDo Task" />;
+  const { state: tasks, dispatch } = useContext(TaskContext);
+
+  const tasksToDo = tasks.filter((task) => task.completed === false);
+  return <LayoutPages title="ToDo Task" tasks={tasksToDo} />;
 }
