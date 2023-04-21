219e23ab83c0ea5d6cd33d44d00d249b1804068d
aliseyedi <solafiali657@gmail.com>
2023-04-22 00:38:09 +0330

    feat ( ModalCategory ) : prevent add category when exist already category

--- src/components/modal/ModalCategory.jsx
+++ src/components/modal/ModalCategory.jsx
@@ -5,19 +5,26 @@ import { CategoryContext } from "../../State/categoryReducer";
 
 export default function ModalCategory({ onClose, title, textButton }) {
   const [newCategory, setNewCategory] = useState("");
-  const { dispatch } = useContext(CategoryContext);
+  const { state: categories, dispatch } = useContext(CategoryContext);
   const searchInputRef = useRef(null);
+  const [isDuplicate, setIsDuplicate] = useState(false);
 
   const NewCategory = (e) => {
     e.preventDefault();
+    if (isDuplicate) {
+      return;
+    }
     dispatch({ type: "ADD_CATEGORY", newCategory: newCategory });
     onClose();
   };
 
-  // console.log(newCategory);
   useEffect(() => {
     searchInputRef.current.focus();
   }, []);
+
+  useEffect(() => {
+    setIsDuplicate(categories.includes(newCategory));
+  }, [newCategory, categories]);
 
   return (
     <Modal onClose={onClose} title={title}>
@@ -26,7 +33,9 @@ export default function ModalCategory({ onClose, title, textButton }) {
           <LabelModal htmlFor="category">Title Category</LabelModal>
           <InputModal
             type="text"
+            autocomplete="off"
             id="category"
+            maxLength={10}
             placeholder="Enter New Category"
             onChange={(e) => {
               setNewCategory(e.target.value);
@@ -34,6 +43,9 @@ export default function ModalCategory({ onClose, title, textButton }) {
             autoFocus
             ref={searchInputRef}
           />
+          {isDuplicate && (
+            <p className="mt-2 text-sm text-red-500">This category already exists.</p>
+          )}
         </div>
         <ButtonModal>{textButton}</ButtonModal>
       </form>
