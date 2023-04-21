d04688585bd5ebad7f531e04c36796e796b99d62
aliseyedi <solafiali657@gmail.com>
2023-04-21 16:12:41 +0330

    feat (modalSearch ) : add modalSearch for Search of Tasks

--- src/components/Task/Header/SearchTask.jsx
+++ src/components/Task/Header/SearchTask.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { InputModal } from "../../customs/@core";
 import { BiSearch } from "react-icons/bi";
+import ModalSearch from "../../modal/ModalSearch";
 
 export default function SearchTask() {
+  const [showModalSearch, setShowModalSearch] = useState(false);
+
   return (
-    <div className=" w-full md:w-max">
-      <button className="y flex w-full items-center justify-between gap-2 rounded-md  bg-slate-300 px-2  py-1 text-base hover:bg-slate-400 dark:bg-slate-300">
-        <BiSearch className="text-red-00  text-xl  " />
-        Quick Search ...
-        <span className="rounded-md  bg-indigo-500 px-2 py-1 text-xs text-slate-200 ">Ctrl K </span>
-      </button>
-    </div>
+    <>
+      {showModalSearch && (
+        <ModalSearch
+          onClose={() => {
+            setShowModalSearch(false);
+          }}
+        />
+      )}
+
+      <div className=" w-full md:w-max">
+        <button
+          className="y flex w-full items-center justify-between gap-2 rounded-md  bg-slate-300 px-2  py-1 text-base hover:bg-slate-400 dark:bg-slate-300"
+          onClick={() => {
+            setShowModalSearch(true);
+          }}
+        >
+          <BiSearch className="text-red-00  text-xl  " />
+          Quick Search ...
+          <span className="rounded-md  bg-indigo-500 px-2 py-1 text-xs text-slate-200 ">
+            Ctrl K{" "}
+          </span>
+        </button>
+      </div>
+    </>
   );
 }
---
+++ src/components/modal/ModalSearch.jsx
@@ -0,0 +1,71 @@
+import React, { useContext, useEffect, useState } from "react";
+import Modal from "./Modal";
+import { InputModal, LabelModal } from "../customs/@core";
+import { BiSearch } from "react-icons/bi";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { BsCheckSquareFill } from "react-icons/bs";
+import { TaskContext } from "../../State/taskReducer";
+import { ImCheckboxUnchecked } from "react-icons/im";
+
+export default function ModalSearch({ onClose }) {
+  const { state: tasks } = useContext(TaskContext);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  //   console.log(tasks);
+  //   console.log("result", searchResults);
+
+  useEffect(() => {
+    if (searchValue === "") {
+      setSearchResults([]);
+    } else {
+      const results = tasks.filter((task) =>
+        task.title.toLowerCase().includes(searchValue.toLowerCase()),
+      );
+      setSearchResults(results);
+    }
+  }, [searchValue, tasks]);
+
+  const searchTaskHandle = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <Modal onClose={onClose} title="Search your Name Task">
+      <LabelModal className="relative">
+        <InputModal
+          type="search"
+          placeholder="Search Task ..."
+          className=" w-full pl-8"
+          onChange={searchTaskHandle}
+        />
+        <BiSearch className="text-red-00  absolute  left-2 top-4 text-xl " />
+      </LabelModal>
+      <div>
+        <ul className="hide-scrollbar max-h-40 overflow-y-scroll rounded-sm ">
+          {searchResults.map((task) => (
+            <li
+              key={task.id}
+              className="mt-1 flex items-center justify-between rounded-sm bg-indigo-500 p-1  px-1  text-white"
+            >
+              <div>
+                <p className="text-base">{task.title}</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <p className="font-Montserrat text-sm ">{task.date}</p>
+
+                {task.important ? (
+                  <AiFillStar className="text-base" />
+                ) : (
+                  <AiOutlineStar className="text-base" />
+                )}
+
+                {task.completed ? <BsCheckSquareFill /> : <ImCheckboxUnchecked />}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Modal>
+  );
+}
